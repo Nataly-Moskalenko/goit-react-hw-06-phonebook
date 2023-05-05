c2ef1cdde6e14ac2a91bbf34dd7687c8c67071d0
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-05-05 13:52:45 +0300

    adds react-toastify

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { addContact, deleteContact } from 'redux/contactsSlice';
 import { changeFilter } from 'redux/filterSlice';
@@ -9,7 +12,6 @@ import ContactList from './contactList/ContactList';
 import Filter from './filter/Filter';
 
 import css from './App.module.css';
-import { useState, useEffect } from 'react';
 
 export function App() {
   const dispatch = useDispatch();
@@ -24,42 +26,45 @@ export function App() {
         contact => contact.name.toLowerCase() === values.name.toLowerCase()
       )
     ) {
-      window.alert(`${values.name} is already in contacts`);
+      toast.info(`${values.name} is already in contacts.`);
     } else {
-      dispatch(addContact(values));      
+      dispatch(addContact(values));
     }
-    resetForm();    
+    resetForm();
   };
- 
+
   const handleDeleteContact = id => {
-    dispatch(deleteContact(id));    
+    dispatch(deleteContact(id));
   };
 
   const handleChangeFilter = e => {
     const filterValue = e.currentTarget.value;
-    dispatch(changeFilter(filterValue));    
+    dispatch(changeFilter(filterValue));
   };
-  
+
   useEffect(() => {
     setVisibleContacts(
       contacts.filter(contact =>
         contact.name.toLowerCase().includes(filter.toLowerCase())
       )
-    );    
-  }, [contacts, filter]);  
+    );
+  }, [contacts, filter]);
 
   return (
     <div className={css.app}>
-      <h1>Phonebook</h1>
-      <ContactForm onSubmit={handleSubmit} />
-      <h2>Contacts</h2>
-      <Filter value={filter} handleChange={handleChangeFilter} />
-      {contacts.length > 0 && (
-        <ContactList
-          contacts={visibleContacts}
-          onDeleteContact={handleDeleteContact}
-        />
-      )}
+      <div className={css.phonebook}>
+        <h1 className={css.phonebook__title}>Phonebook</h1>
+        <ContactForm onSubmit={handleSubmit} />
+        <h2>Contacts</h2>
+        <Filter value={filter} handleChange={handleChangeFilter} />
+        {contacts.length > 0 && (
+          <ContactList
+            contacts={visibleContacts}
+            onDeleteContact={handleDeleteContact}
+          />
+        )}
+      </div>
+      <ToastContainer autoClose={3000} />
     </div>
   );
 }
